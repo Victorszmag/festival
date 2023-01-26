1e28d953edab4a0364d8c9de1f645dce6a540cf8
Victor Szmaglinski <victorszmaglinski@Victors-MBP.attlocal.net>
2023-01-26 03:06:15 -0800

    started Edit

---
+++ views/Edit.jsx
@@ -0,0 +1,10 @@
+const React = require("react");
+
+class Edit extends React.Component {
+    render() {
+        const{name, time, stage} = this.props.festival;
+        return (
+            
+        )
+    }
+}
