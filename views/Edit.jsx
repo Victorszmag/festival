1d258b0b5b9c4623ccad443524d1d1a595d7e2f3
Victor Szmaglinski <victorszmaglinski@Victors-MBP.attlocal.net>
2023-01-30 00:38:58 -0800

    added edit page

--- views/Edit.jsx
+++ views/Edit.jsx
@@ -1,10 +1,31 @@
 const React = require("react");
+const DefaultLayout = require("../layout/Default");
 
 class Edit extends React.Component {
-    render() {
-        const{name, time, stage} = this.props.festival;
-        return (
-            
-        )
-    }
+  render() {
+    const { name, _id, readyToSee, when, where } = this.props.festival;
+
+    return (
+        <DefaultLayout title={`${name} Edit Page`}>
+        <form action={`/festivals/${_id}?_method=PUT`} method="POST">
+          Name: <input type="text" name="name" defaultValue={name} />
+          <br />
+          When: <input type="text" name="when" defaultValue={when} />
+          <br />
+          Where: <input type="text" name="where" defaultValue={where} />
+          <br />
+          Did you see them?:
+          {readyToSee ? (
+            <input type="checkbox" name="readyToSee" defaultChecked />
+          ) : (
+            <input type="checkbox" name="readyToSee" />
+          )}
+          <br />
+          <input type="submit" value="Submit Changes" />
+        </form>
+        </DefaultLayout>
+    );
+  }
 }
+
+module.exports = Edit;
