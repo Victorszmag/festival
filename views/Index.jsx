53148d15ac3283fb73765206aeafba2cf4325fd0
Victor Szmaglinski <victorszmaglinski@Victors-MBP.attlocal.net>
2023-01-30 00:40:11 -0800

    index added

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,14 +1,40 @@
 const React = require("react");
-const DefaultLayout = require("./layout/Default");
-
+const DefaultLayout = require("../layout/Default");
 class Index extends React.Component {
   render() {
+    // method 2: destructure the props
+    const { festivals } = this.props;
+
     return (
-      <DefaultLayout title="Index page">
-        <p>This is boilerplate 😎</p>
+      <DefaultLayout title="Some Festival" cssPath="/css/app.css">
+        <nav>
+          <a href="/festivals/new">Add an Act to your planner</a>
+        </nav>
+        <ul>
+          {this.props.festivals.map((festival, i) => {
+            return (
+              <li key={i}>
+                <a href={`/festivals/${festival.id}`}> {festival.name} </a> is playing at {" "}
+                {festival.when} at the {festival.where} {" "} stage
+                {" - "}
+                {festival.readyToSee
+                  ? `I have seen them`
+                  : `I have not seen them`}
+                <form
+                  action={`/festivals/${festival._id}?_method=DELETE`}
+                  method="POST"
+                >
+                  <input type="submit" value="DELETE" />
+                </form>
+                <form action={`/festivals/${festival._id}/edit`} method="GET">
+                  <input type="submit" value="UPDATE" />
+                </form>
+              </li>
+            );
+          })}
+        </ul>
       </DefaultLayout>
     );
   }
 }
-
 module.exports = Index;
